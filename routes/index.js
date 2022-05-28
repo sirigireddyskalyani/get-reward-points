01ac2a02750e1c6e70b4016efdf5749e1cdc8c5e
sirigireddykalyani <sirigireddyskalyani@gmail.com>
2022-05-28 14:02:46 +0530

    Default Express Generator app is updated as Pure REST Service Application. 1) Removed view, render utilities as project supports JSON type. 2) Updated default index path with JSON response.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({
+    title: 'get-reward-points',
+    type: 'API Service',
+    apis: []
+  });
 });
 
 module.exports = router;
